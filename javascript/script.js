b208bb2cb017cdadc1a6bdbf1d51420f74812769
vitriolfox <vitriol.fox@gmail.com>
2017-10-03 15:18:23 +0200

    finished website

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+console.log('Oy Béla');
+
+var button = document.querySelector('.property button');
+var text = document.querySelector('.property p');
+
+button.addEventListener('click', function() {
+    /* if (text.style.display === 'none'){
+        text.style.display = 'block'
+    } else {
+    text.style.display = 'none';
+    } */
+/* if (text.classList.contains('hidden')) {
+    text.classList.remove('hidden');
+} else {
+    text.classList.add('hidden');
+} */
+text.classList.toggle('hidden');
+
+});
+
